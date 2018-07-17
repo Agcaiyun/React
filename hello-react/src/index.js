be0bc20f2c1c1f0733e7343d0e412e1c54ef30f9
agcaiyun <agcaiyun@163.com>
2018-07-17 09:08:39 +0800

    update react study

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class LikeButton extends Component {
+    constructor() {
+        super()
+        this.state = { isLiked: false }
+    }
+
+    handleClickOnLikeButton() {
+        this.setState({
+            isLiked: !this.state.isLiked
+        })
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClickOnLikeButton.bind(this)}>
+                {this.state.isLiked ? '取消' : '点赞'}
+            </button>
+        )
+    }
+}
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+)
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
