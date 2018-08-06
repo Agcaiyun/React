c0685b548e9c315d3e009e050095821226550643
agcaiyun <agcaiyun@163.com>
2018-08-06 15:00:27 +0800

    commmentApp为commentInput传入回调函数作为props

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,53 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import CommentApp from './CommentApp';
 import registerServiceWorker from './registerServiceWorker';
 
-// class Lesson extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{this.props.lessons.title}</h1>
-//                 <p>{this.props.lessons.description}</p>
-//             </div>
-//         )
-//     }
-// }
-
-// class LessonsList extends Component {
-//     render() {
-//         const users = [
-//             { title: 'Lesson 1: title', description: 'Lesson 1: description' },
-//             { title: 'Lesson 1: title', description: 'Lesson 1: description' },
-//             { title: 'Lesson 1: title', description: 'Lesson 1: description' },
-//             { title: 'Lesson 1: title', description: 'Lesson 1: description' }
-//         ]
-
-//         return (
-//             <div>
-//                 {users.map((user, index) => {
-//                     <Lesson lessons={lessons} />
-//                 })}
-//             </div>
-//         )
-//     }
-// }
-
-class Test extends Component {
-    render() {
-        return (
-            <div>
-                <h1>123</h1>
-            </div>
-        )
-    }
-}
-
 ReactDOM.render(
-    <Test />,
+    <CommentApp />,
     document.getElementById('root')
-)
+);
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
