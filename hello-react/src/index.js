7620ff9d37c7a5f723fb560b752514d00ac6f1b9
agcaiyun <agcaiyun@163.com>
2018-08-08 17:57:29 +0800

    百分比换算器试水

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,8 +4,78 @@ import './index.css';
 import CommentApp from './CommentApp';
 import registerServiceWorker from './registerServiceWorker';
 
+class Input extends Component {
+    constructor() {
+        super()
+        this.state = {
+            originNumber: ''
+        }
+    }
+
+    handleChangeInput(event) {
+        this.setState({
+            originNumber: event.target.value
+        }, () => {
+            if (this.props.onInput) {
+                this.props.onInput(this.state.originNumber)
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    type='number'
+                    value={this.state.originNumber}
+                    onChange={this.handleChangeInput.bind(this)}
+                />
+            </div>
+        )
+    }
+}
+
+class PercentageShower extends Component {
+    render() {
+        return (
+            <div>
+                {
+                    this.props.onPresent
+                }
+            </div>
+        )
+    }
+}
+
+class PercentageApp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            inputNumber: ''
+        }
+    }
+
+    handleInput(originNumber) {
+        this.setState({
+            inputNumber: originNumber * 100
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Input
+                    onInput={this.handleInput.bind(this)}
+                />
+                <PercentageShower onPresent={this.state.inputNumber} />
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
-    <CommentApp />,
+    <PercentageApp />,
     document.getElementById('root')
 );
 
