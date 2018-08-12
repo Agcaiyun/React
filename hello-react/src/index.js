8748f1cf6ff67b64a280de6367b93bd513284232
agcaiyun <agcaiyun@163.com>
2018-08-12 21:55:19 +0800

    finish 组件挂在（二）课后习题

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,70 +3,57 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import CommentApp from './CommentApp';
 import registerServiceWorker from './registerServiceWorker';
-class Clock extends Component {
+
+// getPostData 已经可以直接使用
+// 小提示：本系统可以直接 async/await
+
+// 在点击【刷新】按钮刷新页面时，会先显示‘数据加载中’，然后再显示具体的从
+// getPostDate 函数中获取到的页面内容，所以需要先 setState({content: '数据加载中...'})
+// 然后再将 getPostDate 返回的内容用 setState 渲染到页面中
+class Post extends Component {
     constructor() {
-        super()
-        this.state = {
-            date: new Date()
-        }
-    }
-
-    componentWillMount () {
-        this.timer = setInterval(() => {
-            this.setState({
-                date: new Date()
-            })
-        }, 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timer)
-    }
-
-    render () {
-        return (
-            <div>
-                <h1>
-                    <p>
-                        现在的时间是：
-                    </p>
-                    {
-                        this.state.date.toLocaleTimeString()
-                    }
-                </h1>
-            </div>
-        )
-    }
-}
-
-class Index extends Component {
-    constructor () {
-        super()
-        this.state = {
-            isShowClock:true
-        }
-    }
-
-    isHideClock () {
-        this.setState({
-            isShowClock: !this.state.isShowClock
-        })
-    }
-
-    render () {
-        return (
-            <div>
-                {this.state.isShowClock ? <Clock /> :null}
-            <button onClick={this.isHideClock.bind(this)}>
-                显示或者隐藏闹钟
-            </button>
-            </div>
-        )
-    }
-}
+         super()
+         this.state = {
+             content: '数据加载中...'
+         }
+     }
+ 
+     componentWillMount () {
+       this.setPostContent()
+     }
+     
+     setPostContent () {
+       getPostData().then((postContent) => {
+         this.setState({
+           content: postContent 
+         })
+       })
+     }
+     
+     reload () {
+       this.setState ({
+         content: '数据加载中...'
+       })
+       this.setPostContent()
+     }
+     
+   render () {
+       return (
+         <div>
+             <div className='post-content'>
+             {this.state.content }
+             </div>
+             <button onClick={this.reload.bind(this)}>
+               刷新
+             </button>
+         </div>
+       )
+   }
+ }
+ 
 
 ReactDOM.render(
-    <Index />,
+    <Post />,
     document.getElementById('root')
 );
 
