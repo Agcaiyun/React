b54aa2f3da3d182f93cbdbed19a41f5ce5bca231
agcaiyun <agcaiyun@163.com>
2018-08-14 10:48:56 +0800

    study ref

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,57 +4,24 @@ import './index.css';
 // import CommentApp from './CommentApp';
 import registerServiceWorker from './registerServiceWorker';
 
-// getPostData 已经可以直接使用
-// 小提示：本系统可以直接 async/await
-
-// 在点击【刷新】按钮刷新页面时，会先显示‘数据加载中’，然后再显示具体的从
-// getPostDate 函数中获取到的页面内容，所以需要先 setState({content: '数据加载中...'})
-// 然后再将 getPostDate 返回的内容用 setState 渲染到页面中
 class Post extends Component {
-    constructor() {
-         super()
-         this.state = {
-             content: '数据加载中...'
-         }
-     }
- 
-     componentWillMount () {
-       this.setPostContent()
-     }
-     
-     setPostContent () {
-       getPostData().then((postContent) => {
-         this.setState({
-           content: postContent 
-         })
-       })
-     }
-     
-     reload () {
-       this.setState ({
-         content: '数据加载中...'
-       })
-       this.setPostContent()
-     }
-     
-   render () {
-       return (
-         <div>
-             <div className='post-content'>
-             {this.state.content }
-             </div>
-             <button onClick={this.reload.bind(this)}>
-               刷新
-             </button>
-         </div>
-       )
-   }
- }
- 
+  consoleLogHeight() {
+    console.log(this.p.clientHeight)
+  }
+
+  render() {
+    return (
+      <p ref={(p) => this.p = p} onClick={(e) => this.consoleLogHeight.bind(this)}>
+        {this.props.content}
+      </p>
+    )
+  }
+}
+
 
 ReactDOM.render(
-    <Post />,
-    document.getElementById('root')
+  <Post content={'good lucky'} />,
+  document.getElementById('root')
 );
 
 registerServiceWorker();
