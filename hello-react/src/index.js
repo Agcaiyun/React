bff666d4a4e88c4a30300b60c73e043bee60f10d
agcaiyun <agcaiyun@163.com>
2018-08-27 17:59:25 +0800

    study PropTypess

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
 // import CommentApp from './CommentApp';
 import registerServiceWorker from './registerServiceWorker';
 
-// 普通写法
-// const getDefaultStyledPost = (defaultStyle) => {
-// return (
-// class Post extends React.Component {
-//   render() {
-//     const style = { ...defaultStyle, ...this.props.style }
-//     return (
-//       <p style={style}>Just a test</p>
-//     )
-//   }
-// }
-// )
-// }
+class Comment extends Component {
+  static propTypes = {
+    comment: PropTypes.object
+  }
 
+  render() {
+    const { comment } = this.props
 
-// 函数式组件
-const getDefaultStyledPost = (defaultStyle) => {
-  return props => <p style={{ ...defaultStyle, ...props.style }}>
-    test test test
-  </p>
+    return (
+      <div>
+        <span>
+          {comment.username}
+        </span>
+        <p>
+          {comment.content}
+        </p>
+      </div>
+    )
+  }
 }
 
-const A = getDefaultStyledPost({ color: 'red' })
 
 ReactDOM.render(
   <div>
-    < A style={{ color: 'blue', fontSize: '20px' }} />
-    < A style={{ fontSize: '50px' }} />
+    < Comment />
   </div>,
   document.getElementById('root')
 );
