f1cd098953c356b23da9f5c056f3a0e2b4f3eda0
agcaiyun <agcaiyun@163.com>
2018-08-29 23:50:27 +0800

    merge

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM fromc  'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
 // import CommentApp from './CommentApp';
