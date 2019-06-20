4ed84b10496e06d13d80a50054d41b2781e3a6fe
agcaiyun <agcaiyun@163.com>
2019-06-20 18:04:30 +0800

    study redux

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -2,25 +2,26 @@ import React, { Component } from 'react'
 import store from './store'
 
 export default class TodoList extends Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         inputValue: '',
-    //         list: []
-    //     }
-    // }
+    constructor(props) {
+        super(props)
+        this.state = store.getState()
+
+        // this.handleInputChange = this.handleInputChange.bind(this)
+    }
 
     render() {
-        // const { inputValue, list } = this.state
-        const { inputValue, list } = store
-
+        const { inputValue, list } = this.state
 
         return (
             <div>
-                <input placeholder='to do' value={inputValue} />
+                <input
+                    placeholder='to do'
+                    value={inputValue}
+                // onChange={this.handleInputChange}
+                />
                 <ul>
                     {
-                        list.map(item => {
+                        list.map((item) => {
                             return (
                                 <li>{item}</li>
                             )
@@ -30,4 +31,13 @@ export default class TodoList extends Component {
             </div>
         )
     }
+
+    // handleInputChange(e) {
+    //     const action = {
+    //         type: 'change-input-value',
+    //         value: e.target.value
+    //     }
+
+    //     store.dispatch(action)
+    // }
 }
--- todolist/src/store/reducer.js
+++ todolist/src/store/reducer.js
@@ -1,8 +1,9 @@
 const defaultState = {
     inputValue: 'inputValue',
-    list: [1, 2, 3]
+    list: [1, 2, 3, 4, 5, 6, 7]
 }
 
 export default function (state = defaultState, action) {
+    console.log(action)
     return state
 }
