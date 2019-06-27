029640ea16f1578595435d03d9e238cf78e32e22
agcaiyun <agcaiyun@163.com>
2019-06-27 10:58:48 +0800

    delete Test.js

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList'
-import Test from './Test'
 
 ReactDOM.render(
-    // <TodoList />,
-    <Test />,
+    <TodoList />,
     document.getElementById('root')
 );
