2769eceb20bc5a16611b97cbcf374532fd2ad60d
agcaiyun <agcaiyun@163.com>
2019-08-06 17:29:00 +0800

    test

---
+++ todolist/src/Test.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Test extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                123
+            </div>
+        )
+    }
+}
--- todolist/src/index.js
+++ todolist/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './TodoList'
+import TodoList from './TodoList';
+import Test from './Test'
 
 ReactDOM.render(
-    <TodoList />,
+    <Test />,
     document.getElementById('root')
 );
