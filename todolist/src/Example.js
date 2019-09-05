8216fe4e2b8c023dcd41df475d80423dc81907b4
agcaiyun <agcaiyun@163.com>
2019-09-05 09:59:42 +0800

    change hooks to class components

--- todolist/src/Example.js
+++ todolist/src/Example.js
@@ -1,23 +1,51 @@
-import React, { Fragment, useState } from 'react'
+import React, { Component, Fragment, useState } from 'react'
 
-function Example() {
-    const [count, setCount] = useState(10)
+// function Example() {
+//     const [count, setCount] = useState(10)
 
-    return (
-        <Fragment>
-            <p>
-                {
-                    `You clicked ${count} times`
-                }
-            </p>
+//     return (
+//         <Fragment>
+//             <p>
+//                 {
+//                     `You clicked ${count} times`
+//                 }
+//             </p>
 
-            <button onClick={() => setCount(count + 10)}>
-                {
-                    'Click me'
-                }
-            </button>
-        </Fragment>
-    )
+//             <button onClick={() => setCount(count + 10)}>
+//                 {
+//                     'Click me'
+//                 }
+//             </button>
+//         </Fragment>
+//     )
+// }
+
+class Example extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <p>{`You clicked ${this.state.count} times`}</p>
+                <button onClick={this.handleClick}>{'Click me'}</button>
+            </Fragment>
+        )
+    }
+
+
 }
 
 export default Example;
