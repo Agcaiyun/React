9a1e3d415ee0e1a9212f82a429c28bac9e5260e4
agcaiyun <agcaiyun@163.com>
2019-09-30 13:43:09 +0800

    update App.js

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,6 +1,76 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
+export default function App() {
+	return (
+		<Router>
+			<vid>
+				<Navigation />
+
+				<Switch>
+					<Route path='/about'>
+						<About />
+					</Route>
+					<Route path='/topics'>
+						<Topics />
+					</Route>
+					<Route path='/'>
+						<Home />
+					</Route>
+				</Switch>
+			</vid>
+		</Router>
+	)
+}
+
+function Navigation() {
+	return (
+		<ul>
+			<li>
+				<Link to='/about'>About</Link>
+			</li>
+			<li>
+				<Link to='/topics'>Topics</Link>
+			</li>
+			<li>
+				<Link to='/'>Home</Link>
+			</li>
+		</ul>
+	)
+}
+
+function Topics() {
+	return (
+		<div>
+			<h2>Topics</h2>
+
+			<ul>
+				<li>
+					<Link to={`/topics/components`}>Components</Link>
+				</li>
+				<li>
+					<Link to={`/topics/props-v-state`}>Props v. State</Link>
+				</li>
+			</ul>
+
+			<Switch>
+				<Route path={`/topics/components`}>
+					<Topic />
+				</Route>
+				<Route path={`/topics/props-v-state`}>
+					<h3>Please select a topic.</h3>
+				</Route>
+			</Switch>
+		</div>
+	)
+}
+
+function Topic() {
+	return (
+		<h3>Requested topic ID</h3>
+	)
+}
+
 function Home() {
 	return <h2>Home</h2>
 }
@@ -9,41 +79,4 @@ function About() {
 	return <h2>About</h2>
 }
 
-function Users() {
-	return <h2>Users</h2>
-}
-
-export default function App() {
-	return (
-		<Router>
-			<div>
-				<nav>
-					<ul>
-						<li>
-							<Link to='/'>Home</Link>
-						</li>
-						<li>
-							<Link to='/about'>About</Link>
-						</li>
-						<li>
-							<Link to='/users'>Users</Link>
-						</li>
-					</ul>
-				</nav>
-
-				<Switch>
-					<Route path='/about'>
-						<About />
-					</Route>
-					<Route path='/users'>
-						<Users />
-					</Route>
-					<Route path='/'>
-						<Home />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	)
-}
 
