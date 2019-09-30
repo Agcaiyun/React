2e814e3b25edb9484adc21bba561024defd3407d
agcaiyun <agcaiyun@163.com>
2019-09-30 11:26:46 +0800

    Beginning study react-router

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,87 +1,49 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function Home() {
-	return (
-		<h2>
-			{
-				'Home'
-			}
-		</h2>
-	)
+	return <h2>Home</h2>
 }
 
 function About() {
-	return (
-		<h2>
-			{
-				'About'
-			}
-		</h2>
-	)
+	return <h2>About</h2>
 }
 
-function Topic({ match }) {
-	return (
-		<h3>
-			{
-				`Requested Param: ${match.params.id}`
-			}
-		</h3>
-	)
+function Users() {
+	return <h2>Users</h2>
 }
 
-function Topics({ match }) {
-	return (
-		<Fragment>
-			<h2>
-				{
-					'Topics'
-				}
-			</h2>
-
-			<ul>
-				<li>
-					<Link to={`${match.url}/components`}>Components</Link>
-				</li>
-				<li>
-					<Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-				</li>
-			</ul>
-
-
-		</Fragment>
-	)
-}
-
-function Header() {
-	return (
-		<ul>
-			<li>
-				<Link to='/'>Home</Link>
-			</li>
-			<li>
-				<Link to='/about'>About</Link>
-			</li>
-			<li>
-				<Link to='/topics'>Topics</Link>
-			</li>
-		</ul>
-	)
-}
-
-function App() {
+export default function App() {
 	return (
 		<Router>
-			<Fragment>
-				<Header />
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/about'>About</Link>
+						</li>
+						<li>
+							<Link to='/users'>Users</Link>
+						</li>
+					</ul>
+				</nav>
 
-				<Route exact path='/' component={Home} />
-				<Route path='/about' component={About} />
-				<Route path='/topics' component={Topics} />
-			</Fragment>
+				<Switch>
+					<Route path='/about'>
+						<About />
+					</Route>
+					<Route path='/users'>
+						<Users />
+					</Route>
+					<Route path='/'>
+						<Home />
+					</Route>
+				</Switch>
+			</div>
 		</Router>
 	)
 }
 
-export default AppRouter;
