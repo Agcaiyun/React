c2b9b75dd275dbcb10ba7cd43afa41054400e3e4
agcaiyun <agcaiyun@163.com>
2019-10-22 09:06:17 +0800

    location.pathname 匹配多个<Route>

--- react-router/src/RouteView.js
+++ react-router/src/RouteView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function RouteView() {
     return (
@@ -7,6 +7,15 @@ export default function RouteView() {
             <Route
                 path={["/home/user", "/home/blog", "/home/about"]}
                 render={() => <div>Home/user  or Home/blog  or  Home/about</div>}
+            />
+            {/* location.pathname = /document/all 时，界面将展示 path='/document' path='/document/all' 两个路径 */}
+            <Route
+                path={'/document'}
+                render={() => <h2>Document</h2>}
+            />
+            <Route
+                path={'/document/all'}
+                render={() => <h2>Document All</h2>}
             />
         </Router>
     )
